c88df8d1a766616855be6bb4bbaf387f483b061b
Nep <spica@starry.blue>
2021-02-18 00:09:09 +0900

    Update build.gradle.kts

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,5 +1,6 @@
 plugins {
     kotlin("jvm") version "1.4.30"
+    kotlin("plugin.serialization") version "1.4.30"
     id("com.github.johnrengelman.shadow") version "6.1.0"
 
     id("org.jlleitschuh.gradle.ktlint") version "10.0.0"
@@ -10,7 +11,12 @@ plugins {
 object Versions {
     const val Ktor = "1.5.1"
     const val rssreader = "2.4.1"
+    const val Jsoup = "1.13.1"
     const val Kord = "0.7.0-SNAPSHOT"
+    const val kaml = "0.27.0"
+
+    const val Exposed = "0.29.1"
+    const val SQLiteJDBC = "3.30.1"
 
     const val KotlinLogging = "2.0.4"
     const val Logback = "1.2.3"
@@ -21,8 +27,16 @@ object Versions {
 
 object Libraries {
     const val KtorClientCIO = "io.ktor:ktor-client-cio:${Versions.Ktor}"
+    const val KtorClientSerialization = "io.ktor:ktor-client-serialization:${Versions.Ktor}"
     const val rssreader = "com.apptastic:rssreader:${Versions.rssreader}"
+    const val Jsoup = "org.jsoup:jsoup:${Versions.Jsoup}"
     const val Kord = "dev.kord:kord-core:${Versions.Kord}"
+    const val kaml = "com.charleskorn.kaml:kaml:${Versions.kaml}"
+
+    const val ExposedCore = "org.jetbrains.exposed:exposed-core:${Versions.Exposed}"
+    const val ExposedJDBC = "org.jetbrains.exposed:exposed-jdbc:${Versions.Exposed}"
+    const val ExposedJavaTime = "org.jetbrains.exposed:exposed-java-time:${Versions.Exposed}"
+    const val SqliteJDBC = "org.xerial:sqlite-jdbc:${Versions.SQLiteJDBC}"
 
     const val KotlinLogging = "io.github.microutils:kotlin-logging:${Versions.KotlinLogging}"
     const val LogbackCore = "ch.qos.logback:logback-core:${Versions.Logback}"
@@ -51,8 +65,16 @@ repositories {
 
 dependencies {
     implementation(Libraries.KtorClientCIO)
+    implementation(Libraries.KtorClientSerialization)
     implementation(Libraries.rssreader)
+    implementation(Libraries.Jsoup)
     implementation(Libraries.Kord)
+    implementation(Libraries.kaml)
+
+    implementation(Libraries.ExposedCore)
+    implementation(Libraries.ExposedJDBC)
+    implementation(Libraries.ExposedJavaTime)
+    implementation(Libraries.SqliteJDBC)
 
     implementation(Libraries.KotlinLogging)
     implementation(Libraries.LogbackCore)
