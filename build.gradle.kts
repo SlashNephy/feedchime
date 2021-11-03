9310f2791d2914c0a80d76955ff3642ce5804360
Nep <spica@starry.blue>
2021-11-03 23:07:05 +0900

    Upgrade: Kotlin 1.5.31, Gradle 7.2

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,27 +1,20 @@
 plugins {
-    kotlin("jvm") version "1.5.20"
-    kotlin("plugin.serialization") version "1.5.20"
-    id("com.github.johnrengelman.shadow") version "7.0.0"
-
-    id("org.jlleitschuh.gradle.ktlint") version "10.1.0"
-    id("com.adarshr.test-logger") version "2.1.1"
-    id("net.rdrei.android.buildtimetracker") version "0.11.0"
+    kotlin("jvm") version "1.5.31"
+    kotlin("plugin.serialization") version "1.5.31"
+    id("com.github.johnrengelman.shadow") version "7.1.0"
 }
 
 object Versions {
-    const val Ktor = "1.6.0"
+    const val Ktor = "1.6.5"
     const val Rome = "1.15.0"
-    const val Jsoup = "1.13.1"
+    const val Jsoup = "1.14.3"
     const val kaml = "0.34.0"
 
     const val Exposed = "0.32.1"
     const val SQLiteJDBC = "3.30.1"
 
-    const val KotlinLogging = "2.0.8"
+    const val KotlinLogging = "2.0.11"
     const val Logback = "1.2.3"
-    const val jansi = "2.3.3"
-
-    const val JUnit = "5.7.2"
 }
 
 object Libraries {
@@ -38,8 +31,6 @@ object Libraries {
     const val KotlinLogging = "io.github.microutils:kotlin-logging:${Versions.KotlinLogging}"
     const val LogbackCore = "ch.qos.logback:logback-core:${Versions.Logback}"
     const val LogbackClassic = "ch.qos.logback:logback-classic:${Versions.Logback}"
-    const val Jansi = "org.fusesource.jansi:jansi:${Versions.jansi}"
-    const val JUnitJupiter = "org.junit.jupiter:junit-jupiter:${Versions.JUnit}"
 
     val ExperimentalAnnotations = setOf(
         "kotlinx.coroutines.ExperimentalCoroutinesApi",
@@ -68,11 +59,6 @@ dependencies {
     implementation(Libraries.KotlinLogging)
     implementation(Libraries.LogbackCore)
     implementation(Libraries.LogbackClassic)
-    implementation(Libraries.Jansi)
-
-    testImplementation(kotlin("test"))
-    testImplementation(kotlin("test-junit5"))
-    testImplementation(Libraries.JUnitJupiter)
 }
 
 kotlin {
@@ -92,44 +78,8 @@ kotlin {
         languageSettings.progressiveMode = true
 
         Libraries.ExperimentalAnnotations.forEach {
-            languageSettings.useExperimentalAnnotation(it)
+            languageSettings.optIn(it)
         }
-    }
-}
-
-/*
- * Tests
- */
-
-ktlint {
-    verbose.set(true)
-    outputToConsole.set(true)
-    reporters {
-        reporter(org.jlleitschuh.gradle.ktlint.reporter.ReporterType.CHECKSTYLE)
-    }
-    ignoreFailures.set(true)
-}
-
-buildtimetracker {
-    reporters {
-        register("summary") {
-            options["ordered"] = "true"
-            options["barstyle"] = "ascii"
-            options["shortenTaskNames"] = "false"
-        }
-    }
-}
-
-tasks.withType<Test> {
-    useJUnitPlatform()
-
-    testLogging {
-        showStandardStreams = true
-        events("passed", "failed")
-    }
-
-    testlogger {
-        theme = com.adarshr.gradle.testlogger.theme.ThemeType.MOCHA_PARALLEL
     }
 }
 
@@ -137,7 +87,7 @@ task<JavaExec>("run") {
     dependsOn("build")
 
     group = "application"
-    main = "blue.starry.feedchime.MainKt"
+    mainClass.set("blue.starry.feedchime.MainKt")
     classpath(configurations.runtimeClasspath, tasks.jar)
 }
 
